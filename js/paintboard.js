fa4b7561cec22f65a3b3512b7f08ed1109369652
lio0817 <minjino2005@gmail.com>
2023-10-11 09:08:49 +0900

    2

--- js/paintboard.js
+++ js/paintboard.js
@@ -1,6 +1,31 @@
 /**
-*style.css
+*paintBoard.js
 *최초작성일 : 2023.10.04
 *최초작성자 : 노지민
 */
-console.log(">>>paintBoard.js");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.lineWidth = 5;
+
+ctx.fillStyle = "black"; 
+ctx.fillRect(50, 200, 300, 200);
+
+
+ctx.fillStyle = "darkblue";
+ctx.beginPath();
+ctx.moveTo(50, 200);
+ctx.lineTo(200, 100);
+ctx.lineTo(350, 200);
+ctx.closePath();
+ctx.fill();
+
+ctx.fillStyle = "brown";
+ctx.fillRect(175, 300, 50, 100);
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(80, 250, 80, 50);
+ctx.fillRect(240, 250, 80, 50); 
