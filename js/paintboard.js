9e5989e45e2a5f90b217d8214c9d585ae7d41558
lio0817 <minjino2005@gmail.com>
2023-11-08 09:17:57 +0900

    last

--- js/paintboard.js
+++ js/paintboard.js
@@ -1,22 +1,3 @@
-/**
-* paintBoard.js
-* 최초작성일: 2023.10.04
-* 최초작성자: 곽신아
-* 이력: 2023.10.10 집 그리기
-*       2023.10.11 사람 그리기
-*       2023.10.24 - 캔버스를 클릭할 때마다 선 그리기
-*                  - 여러 색으로 선 그리기
-*                  - 사용자가 마우스를 움직이다가 클릭하면 선 그리기
-*                  - 선을 그릴 때마다 선 색깔 바꾸기
-*                  - 색깔 선택하기
-*       2023.10.25 - 모드 선택하기(채우기/그리기)
-*                  - 캔버스 초기화하기
-*                  - 지우기 기능 추가
-*                  - 배경색에 맞추어 지우기 기능 추가
-*       2023.10.31 - 이미지파일 추가하기
-*       2023.11.01 - 캔버스에 텍스트 입력 추가하기
-*                  - 작업한 이미지 파일로 저장하기
-*/
 
 const canvas = document.getElementById("canvas");
 const lineWidth = document.getElementById("line-width");
@@ -39,14 +20,9 @@ canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 ctx.lineCap = "round";
 
-//flag 변수
 let isPainting = false;
 let isFilling = false;
 
-
-/**
- * 캔버스에 선을 그리는 함수
- */
 function onMouseMove(event){
 
     if (isPainting){
@@ -57,98 +33,59 @@ function onMouseMove(event){
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-/**
- * mouse버튼이 내려갈 때
- */
 function onMouseDown(event){
     isPainting = true;
 }
 
-/**
- * 그리기를 취소한다.
- * 선을 그릴 때마다 선 색깔을 바꾼다.
- */
 function cancelPainting(event){
     isPainting = false;
     ctx.beginPath();
 
 }
 
-/**
- * 선의 두께를 변경한다.
- */
 function onChangeLineWidth(event){
-    //console.log(event.target.value);
     ctx.lineWidth = event.target.value;
 }
 
-/**
- * 색을 변경한다.
- */
 function onClickColor(event){
-    //console.dir(event);
     const colorValue = event.target.dataset.color;
     ctx.strokeStyle = colorValue;
     ctx.fillStyle = colorValue;
     if (isFilling){
         canvasBgColor = colorValue;
     }
-    // console.log(colorValue);
-    // console.log(canvasBgColor);
 }
 
-/**
- * 모드를 변경한다.
- * 채우기면 그리기로
- * 그리기면 채우기로
- */
 function onMode(){
-    //isFilling true
     if (isFilling){
         isFilling = false;
         btnMode.innerText = "채우기";
-    }else {    //isFilling false
+    }else {    
         isFilling = true;
         btnMode.innerText = "그리기";
     }
 }
 
-/**
- * 캔버스를 선택한 색깔로 채운다
- */
 function onClickCanvas(event){
-    //console.log("onClickCanvas", event);
-    //isFilling true
     if (isFilling) {
         ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     }
     
 }
 
-/** 
- * 캔버스를 초기화한다. 
-*/
 function onClickInit(){
     
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
-/**
- * 그린 그림을 지운다.
- */
 function onClickEraser(event){
-    //console.dir(event);
-    //ctx.strokeStyle = "white";
     ctx.strokeStyle = canvasBgColor;
     isFilling = false;
     btnMode.innerText = "채우기"
 
 }
 
-/**
- * 선택한 이미지 파일을 그림판에 추가한다.
- */
 function onChangeFile(event){
     console.log(event);
     const file = event.target.files[0];
@@ -157,32 +94,21 @@ function onChangeFile(event){
     image.src = url ;
     image.onload = function(){
         ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        //drawImage(image, dx, dy, dWidth, dHeight)
-        // inputFile.value = null;
     };
 
 }
 
-/**
- * 캔버스를 더블클릭했을 때 실행하는 함수
- * 사용자가 입력한 텍스트를 캔버스에 표시한다.
- */
 function onDoubleClick(event){
-    // console.dir(event);
     const text = inputText.value;
     if (text !== ""){
         ctx.save();
         ctx.lineWidth = 1;
         ctx.font = "60px 'consolas'";
         ctx.strokeText(text, event.offsetX, event.offsetY);
-        // ctx.fillText(text, event.offsetX, event.offsetY);
         ctx.restore();
     }
 }
 
-/**
- * 작업한 이미지를 파일로 저장한다.
- */
 function saveImage(){
     const url = canvas.toDataURL();
     const a = document.createElement("a");
